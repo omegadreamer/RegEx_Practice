18c9734d7ce2ae94dfd552efacc535267f7af09e
omegadreamer <lolromik04@gmail.com>
2024-09-21 03:03:18 +0300

    Code changes + link to learn site

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 
 import re
 
+
+#  here just using re.FINDALL
 just_text = re.findall("([0-9a-zA-z\W]+)", "just the text! "
                                            "Its still just the text "
                                            "with symbols $#@?? "
@@ -18,17 +20,9 @@ test_date = "1 test May date0445 232 10 August 2077 test date1 55 July sus"
 date = re.compile("[0-9]+ [a-zA-z]+ [0-9]+")  # date in a row in text
 print(date.findall(test_date))
 
-test_url2 = "https://github.com/omegadreamer/RegEx_Practice"
-url = re.compile(r"(/|://)")  # url split
-print(url.split(test_url2))
-
 test_site = "https://github.com"
 site = re.compile("(https|http)(://)([a-zA-z0-9.-_]+)\.(ru|com)")
 print(site.findall(test_site))  # site pattern
-
-test_password = "125656501_!Gg"
-password = re.compile(r"^[a-zA-Z0-9)_\-!?]{8,16}$")  # check reliable password
-print(password.findall(test_password))
 
 test_phone = "+79064922482"
 test_phone_2 = "+7(906)492-24-82"
@@ -36,3 +30,19 @@ phone = re.compile(r"^(\+7|8)(\s?)(\(?)([0-9]{3})(\)?)(\s?)(\d{3})"
                    r"(\s?)(-?)(\d{2})(\s?)(-?)(\d{2})$")  # check ru number
 print(phone.findall(test_phone))
 print(phone.findall(test_phone_2))
+
+
+#  here using re.SPLIT to get list of url components
+test_url2 = "https://github.com/omegadreamer/RegEx_Practice"
+url = re.compile(r"(/|://)")  # url split
+print(url.split(test_url2))
+
+
+#  here using re.MATH to check password
+test_password = "125656501_!Gg"
+password = re.compile(r"^[a-zA-Z0-9)_\-!?]{8,16}$")  # check reliable password
+match_password = password.match(test_password)
+if match_password:
+    print("Password successful")
+else:
+    print("Password unsuccessful")
